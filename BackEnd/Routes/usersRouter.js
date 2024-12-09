19488623871eb312c52609b454519279f12c3b16
Stelio Acácio Mondlane <109701079+Stelio-IN@users.noreply.github.com>
2024-12-09 19:30:28 +0200

    add

--- BackEnd/Routes/usersRouter.js
+++ BackEnd/Routes/usersRouter.js
@@ -6,8 +6,8 @@ const router = Router();
 
 // Rota de criação de usuário com upload de arquivo
 router.post("/", upload.single('file'), usersController.createUser);
-router.post('/register', upload.fields([{ name: 'idCopy', maxCount: 1 }]),usersController.createNannyUser);
-router.post('/login', usersController.loginUser);
+router.post("/register", upload.fields([{ name: 'idCopy', maxCount: 1 }]),usersController.createNannyUser);
+router.post("/login", usersController.loginUser);
 router.get("/", usersController.getAllUsers); // Lista todos os usuários
 router.get("/:user_id", usersController.getUserById); // Obtém os detalhes de um usuário específico
 router.put("/:user_id", usersController.updateUser); // Atualiza um usuário existente
