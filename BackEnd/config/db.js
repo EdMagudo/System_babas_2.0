c1910af136784a40d251b635bc2ea03484a0b422
Edilton Magudo <142337151+EdMagudo@users.noreply.github.com>
2025-01-22 04:08:49 -0800

    Update db.js

--- BackEnd/config/db.js
+++ BackEnd/config/db.js
@@ -1,15 +1,14 @@
-export default  {
-    HOST: '145.223.103.28',
-    USER: 'root',
-    PASSWORD: '',
+export default {
+    HOST: 'localhost',  // ou '127.0.0.1'
+    USER: 'nannies',
+    PASSWORD: '224466',
     DB: 'nannyexpress',
     dialect: 'mysql',
     
-    pool:{
+    pool: {
         max: 5,
         min: 0,
         acquire: 30000,
-        idle: 10000,
+        idle: 10000
     }
 }
-
