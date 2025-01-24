2a9ad991b84770e31b86da827316b623eb8b8710
Edilton Magudo <142337151+EdMagudo@users.noreply.github.com>
2025-01-24 00:34:00 -0800

    solve logout

--- FrontEnd/src/components/Estrutura/Logout.tsx
+++ FrontEnd/src/components/Estrutura/Logout.tsx
@@ -1,13 +1,17 @@
 import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
 
 const Logout = () => {
-  const navigate = useNavigate();
-
   useEffect(() => {
-    localStorage.clear(); // Limpa os dados do usuário
-    navigate('/sign-in'); // Redireciona para a página de login
-  }, [navigate]);
+    // Verifica se o logout já foi processado
+    if (!localStorage.getItem('logoutProcessed')) {
+      localStorage.clear(); // Limpa os dados do usuário
+      localStorage.setItem('logoutProcessed', 'true'); // Marca o logout como processado
+      window.location.reload(); // Faz o reload da página
+    } else {
+      localStorage.removeItem('logoutProcessed'); // Remove a marca para futuros logouts
+      window.location.href = '/sign-in'; // Redireciona para a página de login
+    }
+  }, []);
 
   return null; // O componente não precisa renderizar nada visível
 };
